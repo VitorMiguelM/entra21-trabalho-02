9826c05a30a760254fbc1471f1699cfb8d82f494
Daniel Rodrigo França <danieldrf01@gmail.com>
2018-05-25 17:22:09 -0300

    Cadastro Carros
    validado

--- entra21-trabalho-02/entra21-trabalho-02/Cadastro de carros.cs
+++ entra21-trabalho-02/entra21-trabalho-02/Cadastro de carros.cs
@@ -12,9 +12,133 @@ namespace entra21_trabalho_02
 {
     public partial class Cadastro_de_carros : Form
     {
+        
+
         public Cadastro_de_carros()
         {
             InitializeComponent();
         }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+           
+            //Validação
+
+            try
+            {
+                string nomeDaPessoa = txtNomeDaPessoa.Text;
+            }
+            catch
+            {
+                MessageBox.Show("HAHA muito engraçadinho,apenas palavras/letras xD");
+                txtNomeDaPessoa.Focus();
+                return;
+            }
+            try
+            {
+                string marca = txtMarca.Text;
+            }
+            catch
+            {
+                MessageBox.Show("HAHA muito engraçadinho,apenas palavras/letras xD");
+                txtMarca.Focus();
+                return;
+            }
+            try
+            {
+                double alturaDoCarro = Convert.ToDouble(txtAlturaDoCarro.Text);
+            }
+            catch
+            {
+                MessageBox.Show("HAHA muito engraçadinho,apenas Números xD");
+                txtAlturaDoCarro.Focus();
+                return;
+            }
+            try
+            {
+                double pesoDoCarro = Convert.ToDouble(txtPesoDocarro.Text);
+            }
+            catch
+            {
+                MessageBox.Show("HAHA muito engraçadinho,apenas Números xD");
+                txtCidade.Focus();
+                return;
+            }
+            try
+            {
+                int placaDoCarro = Convert.ToInt32(mtbPlaca.Text);
+            }
+            catch
+            {
+                MessageBox.Show("HAHA muito engraçadinho,apenas Números xD");
+                mtbPlaca.Focus();
+                return;
+            }
+            try
+            {
+                string cidade = txtCidade.Text;
+            }
+            catch
+            {
+                MessageBox.Show("HAHA muito engraçadinho,apenas palavras/letras xD");
+                txtCidade.Focus();
+                return;
+            }
+            try
+            {
+                int cpf = Convert.ToInt32(mtbCPF.Text);
+            }
+            catch
+            {
+                MessageBox.Show("HAHA muito engraçadinho,apenas Números xD");
+                mtbCPF.Focus();
+                return;
+            }
+            try
+            {
+                int idade = Convert.ToInt32(txtIdade.Text);
+            }
+            catch
+            {
+                MessageBox.Show("HAHA muito engraçadinho,apenas Números xD");
+                txtIdade.Focus();
+                return;
+            }
+            try
+            {
+                int rg = Convert.ToInt32(mtbRG.Text);
+            }
+            catch
+            {
+                MessageBox.Show("HAHA muito engraçadinho,apenas Números xD");
+                mtbRG.Focus();
+                return;
+            }
+            try
+            {
+                string email = txtEmail.Text;
+            }
+            catch
+            {
+                MessageBox.Show("HAHA muito engraçadinho,apenas palavras/letras xD");
+                txtEmail.Focus();
+                return;
+            }
+            try
+            {
+                string profissao = txtProfissao.Text;
+            }
+            catch
+            {
+                MessageBox.Show("HAHA muito engraçadinho,apenas palavras/letras xD");
+                txtProfissao.Focus();
+                return;
+            }
+
+
+
+
+
+        } 
     }
 }
