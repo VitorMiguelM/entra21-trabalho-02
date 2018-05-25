b50d5a80ad0d08a33ad30510ff0699b4d02a2a24
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-05-25 16:40:23 -0300

    Criado o design e o algorítimo  do windows form "Principal"

--- entra21-trabalho-02/entra21-trabalho-02/Principal.Designer.cs
+++ entra21-trabalho-02/entra21-trabalho-02/Principal.Designer.cs
@@ -28,12 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCadastroMadeira = new System.Windows.Forms.Button();
+            this.btnCadastroAlimentos = new System.Windows.Forms.Button();
+            this.btnCadastroCaminhoes = new System.Windows.Forms.Button();
+            this.btnCadastroEletrodomesticos = new System.Windows.Forms.Button();
+            this.btnCadastroCarrros = new System.Windows.Forms.Button();
+            this.btnCadastroMoveis = new System.Windows.Forms.Button();
+            this.btnCadastroComputadores = new System.Windows.Forms.Button();
+            this.btnCadstroRoupas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(179, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Principal";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnCadastroMadeira
+            // 
+            this.btnCadastroMadeira.BackColor = System.Drawing.Color.Plum;
+            this.btnCadastroMadeira.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastroMadeira.Location = new System.Drawing.Point(12, 99);
+            this.btnCadastroMadeira.Name = "btnCadastroMadeira";
+            this.btnCadastroMadeira.Size = new System.Drawing.Size(233, 52);
+            this.btnCadastroMadeira.TabIndex = 1;
+            this.btnCadastroMadeira.Text = "Cadastro de madeira";
+            this.btnCadastroMadeira.UseVisualStyleBackColor = false;
+            this.btnCadastroMadeira.Click += new System.EventHandler(this.btnCadastroMadeira_Click);
+            // 
+            // btnCadastroAlimentos
+            // 
+            this.btnCadastroAlimentos.BackColor = System.Drawing.Color.Plum;
+            this.btnCadastroAlimentos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastroAlimentos.Location = new System.Drawing.Point(263, 99);
+            this.btnCadastroAlimentos.Name = "btnCadastroAlimentos";
+            this.btnCadastroAlimentos.Size = new System.Drawing.Size(233, 52);
+            this.btnCadastroAlimentos.TabIndex = 2;
+            this.btnCadastroAlimentos.Text = "Cadastro de alimentos";
+            this.btnCadastroAlimentos.UseVisualStyleBackColor = false;
+            this.btnCadastroAlimentos.Click += new System.EventHandler(this.btnCadastroAlimentos_Click);
+            // 
+            // btnCadastroCaminhoes
+            // 
+            this.btnCadastroCaminhoes.BackColor = System.Drawing.Color.Plum;
+            this.btnCadastroCaminhoes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastroCaminhoes.Location = new System.Drawing.Point(12, 169);
+            this.btnCadastroCaminhoes.Name = "btnCadastroCaminhoes";
+            this.btnCadastroCaminhoes.Size = new System.Drawing.Size(233, 52);
+            this.btnCadastroCaminhoes.TabIndex = 3;
+            this.btnCadastroCaminhoes.Text = "Cadastro de caminhões";
+            this.btnCadastroCaminhoes.UseVisualStyleBackColor = false;
+            this.btnCadastroCaminhoes.Click += new System.EventHandler(this.btnCadastroCaminhoes_Click);
+            // 
+            // btnCadastroEletrodomesticos
+            // 
+            this.btnCadastroEletrodomesticos.BackColor = System.Drawing.Color.Plum;
+            this.btnCadastroEletrodomesticos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastroEletrodomesticos.Location = new System.Drawing.Point(263, 169);
+            this.btnCadastroEletrodomesticos.Name = "btnCadastroEletrodomesticos";
+            this.btnCadastroEletrodomesticos.Size = new System.Drawing.Size(233, 52);
+            this.btnCadastroEletrodomesticos.TabIndex = 4;
+            this.btnCadastroEletrodomesticos.Text = "Cadastro de eletrodomésticos";
+            this.btnCadastroEletrodomesticos.UseVisualStyleBackColor = false;
+            this.btnCadastroEletrodomesticos.Click += new System.EventHandler(this.btnCadastroEletrodomesticos_Click);
+            // 
+            // btnCadastroCarrros
+            // 
+            this.btnCadastroCarrros.BackColor = System.Drawing.Color.Plum;
+            this.btnCadastroCarrros.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastroCarrros.Location = new System.Drawing.Point(12, 237);
+            this.btnCadastroCarrros.Name = "btnCadastroCarrros";
+            this.btnCadastroCarrros.Size = new System.Drawing.Size(233, 52);
+            this.btnCadastroCarrros.TabIndex = 5;
+            this.btnCadastroCarrros.Text = "Cadastro de carros";
+            this.btnCadastroCarrros.UseVisualStyleBackColor = false;
+            this.btnCadastroCarrros.Click += new System.EventHandler(this.btnCadastroCarrros_Click);
+            // 
+            // btnCadastroMoveis
+            // 
+            this.btnCadastroMoveis.BackColor = System.Drawing.Color.Plum;
+            this.btnCadastroMoveis.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastroMoveis.Location = new System.Drawing.Point(263, 237);
+            this.btnCadastroMoveis.Name = "btnCadastroMoveis";
+            this.btnCadastroMoveis.Size = new System.Drawing.Size(233, 52);
+            this.btnCadastroMoveis.TabIndex = 6;
+            this.btnCadastroMoveis.Text = "Cadastro de móveis";
+            this.btnCadastroMoveis.UseVisualStyleBackColor = false;
+            this.btnCadastroMoveis.Click += new System.EventHandler(this.btnCadastroMoveis_Click);
+            // 
+            // btnCadastroComputadores
+            // 
+            this.btnCadastroComputadores.BackColor = System.Drawing.Color.Plum;
+            this.btnCadastroComputadores.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastroComputadores.Location = new System.Drawing.Point(12, 304);
+            this.btnCadastroComputadores.Name = "btnCadastroComputadores";
+            this.btnCadastroComputadores.Size = new System.Drawing.Size(233, 52);
+            this.btnCadastroComputadores.TabIndex = 7;
+            this.btnCadastroComputadores.Text = "Cadastro de computadores";
+            this.btnCadastroComputadores.UseVisualStyleBackColor = false;
+            this.btnCadastroComputadores.Click += new System.EventHandler(this.btnCadastroComputadores_Click);
+            // 
+            // btnCadstroRoupas
+            // 
+            this.btnCadstroRoupas.BackColor = System.Drawing.Color.Plum;
+            this.btnCadstroRoupas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadstroRoupas.Location = new System.Drawing.Point(263, 304);
+            this.btnCadstroRoupas.Name = "btnCadstroRoupas";
+            this.btnCadstroRoupas.Size = new System.Drawing.Size(233, 52);
+            this.btnCadstroRoupas.TabIndex = 8;
+            this.btnCadstroRoupas.Text = "Cadastro de roupas";
+            this.btnCadstroRoupas.UseVisualStyleBackColor = false;
+            this.btnCadstroRoupas.Click += new System.EventHandler(this.btnCadstroRoupas_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Orchid;
+            this.ClientSize = new System.Drawing.Size(508, 382);
+            this.Controls.Add(this.btnCadstroRoupas);
+            this.Controls.Add(this.btnCadastroComputadores);
+            this.Controls.Add(this.btnCadastroMoveis);
+            this.Controls.Add(this.btnCadastroCarrros);
+            this.Controls.Add(this.btnCadastroEletrodomesticos);
+            this.Controls.Add(this.btnCadastroCaminhoes);
+            this.Controls.Add(this.btnCadastroAlimentos);
+            this.Controls.Add(this.btnCadastroMadeira);
+            this.Controls.Add(this.label1);
+            this.Name = "Principal";
+            this.Text = "Cadastros de carros";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCadastroMadeira;
+        private System.Windows.Forms.Button btnCadastroAlimentos;
+        private System.Windows.Forms.Button btnCadastroCaminhoes;
+        private System.Windows.Forms.Button btnCadastroEletrodomesticos;
+        private System.Windows.Forms.Button btnCadastroCarrros;
+        private System.Windows.Forms.Button btnCadastroMoveis;
+        private System.Windows.Forms.Button btnCadastroComputadores;
+        private System.Windows.Forms.Button btnCadstroRoupas;
     }
 }
 
--- entra21-trabalho-02/entra21-trabalho-02/Principal.cs
+++ entra21-trabalho-02/entra21-trabalho-02/Principal.cs
@@ -16,5 +16,58 @@ namespace entra21_trabalho_02
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastroMadeira_Click(object sender, EventArgs e)
+        {
+            Cadastro_de_alimentos cadastro_de_alimentos = new Cadastro_de_alimentos();
+            cadastro_de_alimentos.Show();
+        }
+
+        private void btnCadastroAlimentos_Click(object sender, EventArgs e)
+        {
+            Cadastro_de_alimentos cadastro_de_alimentos = new Cadastro_de_alimentos();
+            cadastro_de_alimentos.Show();
+        }
+
+        private void btnCadastroCaminhoes_Click(object sender, EventArgs e)
+        {
+            Cadastro_de_caminhões cadastro_de_caminhoes = new Cadastro_de_caminhões();
+            cadastro_de_caminhoes.Show();
+        }
+
+        private void btnCadastroEletrodomesticos_Click(object sender, EventArgs e)
+        {
+            Cadastro_de_eletrodomésticos cadastro_de_eletrodomésticos = new Cadastro_de_eletrodomésticos();
+            cadastro_de_eletrodomésticos.Show();
+        }
+
+        private void btnCadastroCarrros_Click(object sender, EventArgs e)
+        {
+            Cadastro_de_carros cadastro_de_carros = new Cadastro_de_carros();
+            cadastro_de_carros.Show();
+        }
+
+        private void btnCadastroMoveis_Click(object sender, EventArgs e)
+        {
+            Cadastro_de_móveis cadastro_de_moveis = new Cadastro_de_móveis();
+            cadastro_de_moveis.Show();
+        }
+
+        private void btnCadastroComputadores_Click(object sender, EventArgs e)
+        {
+            Cadastro_de_computadores cadastro_de_cmputadores = new Cadastro_de_computadores();
+            cadastro_de_cmputadores.Show();
+        }
+
+        private void btnCadstroRoupas_Click(object sender, EventArgs e)
+        {
+            Cadastro_de_roupas cadastro_de_roupas = new Cadastro_de_roupas();
+            cadastro_de_roupas.Show();
+        }
     }
 }
