b9240bd4aea1e79a443b07b191a165f6d4ca4898
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-05-29 16:25:31 -0300

    criado o W.F "Lista de madeira"

---
+++ entra21-trabalho-02/entra21-trabalho-02/Madeira.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entra21_trabalho_02
+{
+    public class Madeira
+    {
+        public string LocalDeExtracao;
+        public int Demanda;
+        public int TempoDeExistencia;
+        public double PrecoPorUnidade;
+        public string Destino;
+        public int QuantidadeDeCaminhoesDisponiveis;
+    }
+}
